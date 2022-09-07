9977d21ed6c290c9c4f046069c794592186664aa
Stefano Izzo <45483547+italianpwner@users.noreply.github.com>
2022-09-07 16:06:51 +0200

    Parallel processing added to 'comuni.js'

--- scraping/comuni.js
+++ scraping/comuni.js
@@ -1,26 +1,18 @@
-import progressBar from './progress-bar.js'
-import fetch from 'node-fetch'
+import WorkerPool from './worker-pool.js'
+import fetch from './fetch.js'
 import cheerio from 'cheerio'
 import fs from 'fs'
- 
-const html = (URL) => {
-	return fetch(URL)
-			.then((resp) => resp.arrayBuffer())
-			.then((data) => {
-				return new TextDecoder('ISO-8859-1').decode(data)
-			})
-}
 
 const filePath = 'data/comuni.json'
-const URL = "http://www.comuni-italiani.it/cap/"
+const url = "http://www.comuni-italiani.it/cap/"
+const suffixes = []
+const results = []
+const tasks = []
 
 if( ! fs.existsSync(filePath)) {
 	console.log("Scraping 'comuni' avviato...")
-	
-	const suffixes = []
-	let results = []
-	
-	let $ = cheerio.load(await html(URL))
+
+	let $ = cheerio.load(await fetch.html(url, 'ISO-8859-1'))
 	
 	const rows = $("table.tabwrap")[0]
 			.children[1].children
@@ -36,36 +28,22 @@ if( ! fs.existsSync(filePath)) {
 		})
 	}
 	
-	progressBar.init(suffixes.length)
+	const pool = new WorkerPool('./scraping/worker-comuni.js', suffixes.length)
 	
-	for(const suffix of suffixes) {
-		
-		$ = cheerio.load(await html(URL+suffix))
-		
-		const rows = $("table.tabwrap")[0]
-				.children[1].children
-				.filter((el) => el.name === "tr")
-		
-		for(let i=0; i<rows.length; i++) {
-			const cols = rows[i].children
-					.filter((el) => el.name === "td")
-			const n = (cols.length === 4) ? 2 : 1
-			
-			for(let j=0; j<n; j++) {
-				let key = cols[j*2].children[0].data
-				const val = cols[j*2+1].children[0].children[0]
-				
-				if(key.includes("x")) {
-					$ = cheerio.load(await html(URL+val.attribs.href))
-					key = $("td.ival")[3].children[0].children[0].data
-				}
-				
-				results.push(key+":"+val.children[0].data)
-			}
-		} 
-		progressBar.update()
+	for(let i=0; i<suffixes.length; i++) {
+		tasks.push(new Promise((resolve, reject) => {
+			const suffix = suffixes[i]
+			pool.runTask({ url, suffix }, (err, res) => {
+				if(err)	return reject(err)
+				results[i] = res
+				return resolve(res)
+			})
+		}))
 	}
-	
-	fs.writeFile(filePath, JSON.stringify(results), () => {})
-}
-console.log("Scraping 'comuni' completato (disponibile qui: '"+filePath+"')")
+
+	Promise.all(tasks).then(() => {
+		[].concat.apply([], results)
+		fs.writeFile(filePath, JSON.stringify(results[0]), () => {})
+		console.log("Scraping 'comuni' completato (disponibile qui: '"+filePath+"')")
+	})
+} else console.log("Scraping 'comuni' completato (disponibile qui: '"+filePath+"')")
---
+++ scraping/worker-comuni.js
@@ -0,0 +1,40 @@
+import { parentPort } from 'worker_threads'
+import fetch from './fetch.js'
+import cheerio from 'cheerio'
+
+parentPort.on('message', (task) => {
+	scrape(task.url, task.suffix).then((res) => {
+		parentPort.postMessage(res)
+	})
+})
+
+const scrape = async (url, suffix) => {
+	const results = []
+	let $ = cheerio.load(await fetch.html(url+suffix, 'ISO-8859-1'))
+	
+	const rows = $("table.tabwrap")[0]
+			.children[1].children
+			.filter((el) => el.name === "tr")
+	
+	for(let i=0; i<rows.length; i++) {
+		const cols = rows[i].children
+				.filter((el) => el.name === "td")
+		const n = (cols.length === 4) ? 2 : 1
+		
+		for(let j=0; j<n; j++) {
+			let key = cols[j*2].children[0].data
+			const val = cols[j*2+1].children[0].children[0]
+
+			if(key.includes("x")) {
+				$ = cheerio.load(await fetch.html(url+val.attribs.href, 'ISO-8859-1'))
+				key = $("td.ival")[3].children[0].children[0].data
+			}
+			
+			results.push(key+":"+val.children[0].data)
+		}
+	}
+	return results
+}
+
+
+
